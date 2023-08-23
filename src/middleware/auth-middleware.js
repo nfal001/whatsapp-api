e5c8a8ab52eb3c6d067e7ea9b3dc63d737538a09
nfal001 <abdurahman.nfal@gmail.com>
2023-08-23 22:57:26 +0700

    auth changes, make it more standard - using Bearer for Auth type in Authorization - encode to uuid to base64 ( temporary, should using bcrypt nextUpdate ) - Lint

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -2,15 +2,16 @@ import { prismaClient } from "../application/database.js";
 
 export const authMiddleware = async (req, res, next) => {
     const token = req.get("Authorization");
+    const authToken = token.split(" ")
 
-    if(!token) {
+    if (!token || authToken[0] !== 'Bearer') {
         res.status(401).json({
             errors: "Unauthorized"
         }).end();
     } else {
         const user = await prismaClient.user.findFirst({
             where: {
-                token: token
+                token: authToken[1]
             }
         });
 
