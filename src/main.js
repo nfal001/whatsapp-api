32ffb984a822ee3ab6d04a4696ca55862f6b856e
nfal001 <abdurahman.nfal@gmail.com>
2023-09-05 14:19:43 +0700

    clean up header response - benerin biar enak

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import 'dotenv/config'
 import { logger } from "./application/logging.js"
 import { web } from "./application/web.js"
+import config from '../src/config/config.js';
 
-web.listen(3000, () => {
+web.disable('x-powered-by')
+
+web.listen(config.app.port, () => {
     logger.info("App Start");
+    console.log("Listening to 127.0.0.1:" + config.app.port);
 });
