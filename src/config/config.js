803c1050081b2bcc72b62c589de37cf001f8f0f9
nfal001 <abdurahman.nfal@gmail.com>
2023-09-07 18:09:58 +0700

    fix config headless mode

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,7 @@ const DEBUG = process.env.DEBUG === 'true' ? true : false
 const PORT = process.env.APP_PORT || 8080
 const APP_ENV = process.env.APP_ENV || 'development'
 const LOCAL_AUTH = process.env.LOCAL_AUTH || process.cwd() + '/storage/whatsapp/local_auth/'
+const BROWSER_HEADLESS = process.env.BROWSER_HEADLESS === 'true' ? true : false
 
 const config = {
     app: {
@@ -12,10 +13,14 @@ const config = {
     },
     bot: {
         prefix: 'w!',
+        browser: {
+            headless: BROWSER_HEADLESS
+        },
         command: {
             ping: 'ping',
             timeNow: 'now'
         }
     }
 }
+
 export default config 
